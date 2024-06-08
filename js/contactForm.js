5f027674427ba9084cc2674473da2e0d20e0219e
tmoshole <tshepomosh31@gmail.com>
2024-06-08 15:47:57 +0200

    chore:updating site

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,21 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Initialize EmailJS with your User ID
-    emailjs.init("YOUR_USER_ID"); // Replace with your EmailJS user ID
+document.addEventListener("DOMContentLoaded", () => {
+  emailjs.init("_YNRMnSZX5diHYYGa" );
 
-    const form = document.getElementById('contactForm');
-    const formResponse = document.getElementById('formResponse');
+  const form = document.getElementById("contactForm");
+  const formResponse = document.getElementById("formResponse");
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        
-        emailjs.sendForm(service_ruyy3wl, template_qw2066h, this)
-            .then(function(response) {
-                console.log('SUCCESS!', response.status, response.text);
-                formResponse.textContent = "Thank you for your message!";
-                form.reset();
-            }, function(error) {
-                console.log('FAILED...', error);
-                formResponse.textContent = "There was an error. Please try again.";
-            });
-    });
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    emailjs.sendForm("service_424dmxm", "template_qw2066h", this).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        showToast();
+        form.reset();
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        formResponse.textContent = "There was an error. Please try again.";
+      }
+    );
+  });
+
+  
+// Function to show toast message
+function showToast() {
+    toast.className = "toast show";
+    setTimeout(() => { toast.className = toast.className.replace("show", ""); }, 2000);
+}
+
+
 });
